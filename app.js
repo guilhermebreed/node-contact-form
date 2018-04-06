27e29b281b43d4af9375f00fa0d1158700d75363
guilhermebreed <guilherme.bralves@hotmail.com>
2018-04-06 13:09:57 -0300

    Removendo dados importantes

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ app.post('/send', (req, res) => {
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
-            user: 'guilhermebreed@gmail.com', // generated ethereal user
-            pass: '31101967' // generated ethereal password
+            user: '', // generated ethereal user
+            pass: '' // generated ethereal password
         },
         tls:{
             rejectUnauthorized: false
